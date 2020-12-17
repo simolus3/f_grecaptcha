c95f939b6ef3de5f4d57224c31019a2c1189baf1
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2020-12-17 15:26:57 +0530

    major revamp - [f_grecaptcha] android side upgrades   - upgrade gradle   - upgrade compileSdk to 29   - migrate to androidX   - upgraded safetynet dependencies   - implemented embedding v2 - [example] android side upgrade   - upgrade gradle   - upgrade compileSdk to 29   - migrate to androidX   - moved to embedding v2

--- android/src/main/java/de/simolus3/fgrecaptcha/FGrecaptchaPlugin.java
+++ android/src/main/java/de/simolus3/fgrecaptcha/FGrecaptchaPlugin.java
@@ -1,55 +1,104 @@
 package de.simolus3.fgrecaptcha;
 
-import android.support.annotation.NonNull;
+import android.app.Activity;
 
 import com.google.android.gms.safetynet.SafetyNet;
 import com.google.android.gms.safetynet.SafetyNetApi;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-public class FGrecaptchaPlugin implements MethodCallHandler {
+public class FGrecaptchaPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler {
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
 
-	private Registrar registrar;
+    // activity required to call SafetyNet API
+    private Activity activity;
 
-	private FGrecaptchaPlugin(Registrar registrar) {
-		this.registrar = registrar;
-	}
+    // default constructor required by Flutter
+    FGrecaptchaPlugin() {
+    }
 
-	public static void registerWith(Registrar registrar) {
-		final MethodChannel channel = new MethodChannel(registrar.messenger(), "f_grecaptcha");
-		channel.setMethodCallHandler(new FGrecaptchaPlugin(registrar));
-	}
+    // this constructor required for static registerWith
+    FGrecaptchaPlugin(Activity activity) {
+        this.activity = activity;
+    }
 
-	@Override
-	public void onMethodCall(MethodCall call, final Result result) {
-		if (call.method.equals("isSupported")) {
-			result.success(true);
-		} else if (call.method.equals("verify")) {
-			String siteKey = call.argument("key");
+    // static registerWith required to support older v1 Android Embeddings
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "f_grecaptcha");
+        channel.setMethodCallHandler(new FGrecaptchaPlugin(registrar.activity()));
+    }
 
-			SafetyNet.getClient(registrar.activity()).verifyWithRecaptcha(siteKey)
-				.addOnSuccessListener(new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
-					@Override
-					public void onSuccess(SafetyNetApi.RecaptchaTokenResponse response) {
-						result.success(response.getTokenResult());
-					}
-				})
-				.addOnFailureListener(new OnFailureListener() {
-					@Override
-					public void onFailure(@NonNull Exception e) {
-						e.printStackTrace();
-						result.error("f_grecaptcha",
-								"Verification using reCaptcha has failed", null);
-					}
-				});
-		} else {
-			result.notImplemented();
-		}
-	}
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "f_grecaptcha");
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        activity = null;
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, final Result result) {
+        if (call.method.equals("isSupported")) {
+            result.success(true);
+        } else if (call.method.equals("verify")) {
+            if (activity == null) {
+                result.error("f_grecaptcha", "No activity found, this plugin works with Activity only.", null);
+                return;
+            }
+            String siteKey = call.argument("key");
+
+            SafetyNet.getClient(activity).verifyWithRecaptcha(siteKey)
+                    .addOnSuccessListener(new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
+                        @Override
+                        public void onSuccess(SafetyNetApi.RecaptchaTokenResponse response) {
+                            result.success(response.getTokenResult());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            e.printStackTrace();
+                            result.error("f_grecaptcha",
+                                    "Verification using reCaptcha has failed", null);
+                        }
+                    });
+        } else {
+            result.notImplemented();
+        }
+    }
 }
